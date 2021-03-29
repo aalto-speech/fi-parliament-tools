09efbeecee5c066f5a3272ba82729fa38c9573b5
Anja <anja.virkkunen@aalto.fi>
2021-03-29 14:51:05 +0300

    Load ctm_edits.segmented

--- src/fi_parliament_tools/speakerAligner/match.py
+++ src/fi_parliament_tools/speakerAligner/match.py
@@ -1,33 +1,61 @@
 """Functions for matching the speaker turns in Kaldi CTMs and segments files."""
 from typing import Tuple
 
-
-def convert_timestamp(
-    segment_begin: float,
-    subsegment_begin: float,
-) -> float:
-    """Compute the begin of a subsegment relative to the main session (in seconds).
-
-    Args:
-        segment_begin (float): begin time of the uniform segment Kaldi created for decoding
-        subsegment_begin (float): begin time of a word/segmentation result within uniform segment
-
-    Returns:
-        float: the start of the word/segmentation in the main session
-    """
-    return segment_begin + subsegment_begin
+import numpy as np
+import pandas as pd
 
 
-def split_segment_id(segment_id: str) -> Tuple[str, float, float, int]:
+def split_segment_id(segment_id: str) -> Tuple[float, float, int]:
     """Split a segment ID to session name, segment begin, segment end, and word id.
 
     Args:
         segment_id (str): Segment ids are in the form session-001-2015-START-END[WORD_ID]
 
     Returns:
-        Tuple[str, float, float, int]: session name, start, end, and word id
+        Tuple[float, float, int]: start, end, and word id
     """
-    session_id, begin, end = segment_id.rsplit("-", 2)
+    _, begin, end = segment_id.rsplit("-", 2)
     end, word_id = end.split("[")
     word_id = word_id.replace("]", "")
-    return session_id, float(begin) / 100.0, float(end) / 100.0, int(word_id)
+    return float(begin) / 100.0, float(end) / 100.0, int(word_id)
+
+
+def load_to_dataframe(filename: str) -> pd.DataFrame:
+    """Load a ctm_edits.segmented file to a DataFrame and prepare it for realignment.
+
+    Args:
+        filename (str): the file to read
+
+    Returns:
+        pd.DataFrame: segmentation data for realignment
+    """
+    cols = [
+        "session",
+        "ch",
+        "word_start",
+        "word_duration",
+        "asr",
+        "prob",
+        "transcript",
+        "edit",
+        "taint",
+        "tmpA",
+        "tmpB",
+    ]
+    df = pd.read_csv(filename, sep=" ", names=cols)
+    df = df.replace(np.nan, "", regex=True)
+    df = df.assign(kaldi_start="", kaldi_end="")
+
+    for old_col in ("taint", "tmpA", "tmpB"):
+        for substring, new_col in (("start-", "kaldi_start"), ("end-", "kaldi_end")):
+            mask = df[old_col].str.startswith(substring)
+            df.loc[mask, new_col] = df.loc[mask, old_col]
+            if old_col == "taint":
+                df.loc[mask, old_col] = ""
+
+    df[["seg_start", "seg_end", "word_id"]] = df.session.apply(
+        lambda x: pd.Series(split_segment_id(x))
+    )
+    df["session_start"] = df["seg_start"] + df["word_start"]
+    df.drop(columns=["session", "ch", "prob", "tmpA", "tmpB"], inplace=True)
+    return df
--- tests/test_match.py
+++ tests/test_match.py
@@ -5,35 +5,41 @@ from fi_parliament_tools.speakerAligner import match
 
 
 @pytest.mark.parametrize(
-    "segment_begin, subsegment_begin, true_start",
+    "segment_id, true_start, true_end, true_word_index",
     [
-        (0.0, 0.0, 0.0),
-        (870.0, 40.32, 910.32),
-    ],
-)
-def test_timestamp_conversion(
-    segment_begin: float, subsegment_begin: float, true_start: float
-) -> None:
-    """Check that conversions from segment timestamps to the full session are accurate."""
-    converted_start = match.convert_timestamp(segment_begin, subsegment_begin)
-    assert converted_start == true_start
-
-
-@pytest.mark.parametrize(
-    "segment_id, true_main_id, true_start, true_end, true_word_index",
-    [
-        ("session-003-2016-00000000-00090000[165]", "session-003-2016", 0.0, 900.0, 165),
-        ("session-005-2019-00087000-00178692[1955]", "session-005-2019", 870.0, 1786.92, 1955),
-        ("session-069-2015-02871000-02950494[737]", "session-069-2015", 28710.0, 29504.94, 737),
-        ("session-144-2017-05220000-05310000[8]", "session-144-2017", 52200.0, 53100.0, 8),
+        ("session-003-2016-00000000-00090000[165]", 0.0, 900.0, 165),
+        ("session-005-2019-00087000-00178692[1955]", 870.0, 1786.92, 1955),
+        ("session-069-2015-02871000-02950494[737]", 28710.0, 29504.94, 737),
+        ("session-144-2017-05220000-05310000[8]", 52200.0, 53100.0, 8),
     ],
 )
 def test_split_segment_id(
-    segment_id: str, true_main_id: str, true_start: float, true_end: float, true_word_index: int
+    segment_id: str, true_start: float, true_end: float, true_word_index: int
 ) -> None:
     """Check that the segment id is split correctly."""
-    main_id, start, end, word_index = match.split_segment_id(segment_id)
-    assert main_id == true_main_id
+    start, end, word_index = match.split_segment_id(segment_id)
     assert start == true_start
     assert end == true_end
     assert word_index == true_word_index
+
+
+def test_read_table() -> None:
+    """Perform a surface level check that a ctm_edits.segmented file is correctly loaded."""
+    df = match.load_to_dataframe("generated/realign_tmp/05h/session-129-2016_ctm_edits.segmented")
+    assert list(df.columns) == [
+        "word_start",
+        "word_duration",
+        "asr",
+        "transcript",
+        "edit",
+        "taint",
+        "kaldi_start",
+        "kaldi_end",
+        "seg_start",
+        "seg_end",
+        "word_id",
+        "session_start",
+    ]
+    assert df.isna().sum().sum() == 0
+    assert list(df.taint.unique()) == ["", "tainted", "do-not-include-in-text"]
+    assert list(df.edit.unique()) == ["ins", "fix", "sil", "cor", "del", "sub"]
