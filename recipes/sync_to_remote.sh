c60c2c315a031a406d976fba71b9e856eb85d281
Anja <anja.virkkunen@aalto.fi>
2021-10-29 11:57:36 +0300

    Update remote sync scripts.

--- recipes/sync_to_remote.sh
+++ recipes/sync_to_remote.sh
@@ -2,11 +2,12 @@
 
 if [ $# -ne 2 ]; then
 	echo "Usage: recipes/sync_to_remote.sh <from_dir> <to_dir>"
-	echo "e.g.: $0 /path/to/local/corpus_folder /path/to/remote/corpus_folder"
+	echo "e.g.: $0 /path/to/local/corpus_folder/ /path/to/remote/corpus_folder"
 	echo
 	echo "This script synchronizes the contents of the corpus folder (.wav, .text, & .mp4)"
 	echo "to a remote server using rsync. Needed only if you use a computing cluster for"
-	echo "the decoding and segmentation part."
+	echo "the decoding and segmentation part. BE aware that you need the slash at the end"
+	echo "of the <from_dir> parameter because of rsync!"
 	echo
 	exit 1
 fi
@@ -27,7 +28,7 @@ rsync -Phauv --include "*/" --include "*.mp4" --exclude "*" -e "ssh" --chmod=g+s
 # -P outputs progress and makes it possible to continue an interrupted rsync which is helpful with big transfers
 # -h human-readable file sizes and transfer speeds
 # -a keep permissions and timestamps
-# -u prevents overwrite of files that have been changed at Triton
+# -u prevents overwrite of files that have been changed at remote
 # -v verbose output
 # -z compress files before transfer to save bandwith
 
